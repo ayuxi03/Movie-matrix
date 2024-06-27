40b0090e1b727e9f274eb74f05f6a0c6adda2aec
ayuxi03 <91673671+ayuxi03@users.noreply.github.com>
2024-06-27 16:37:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-//APIKEY = 6d2457bf34d319edab10c57f345ff2e9
-//API READ ACCESS TOKEN = eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZDI0NTdiZjM0ZDMxOWVkYWIxMGM1N2YzNDVmZjJlOSIsInN1YiI6IjY2NWEyYTYxODYwMzY5NWQxYjUwOWRmOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VY18n8YJ6eoq3MQFRTL-wV4SHSIRPQYfj82hbIiDNiI
-
 const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6d2457bf34d319edab10c57f345ff2e9&page=1';
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=6d2457bf34d319edab10c57f345ff2e9&query=";
